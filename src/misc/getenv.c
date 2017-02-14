27a2f98fe4cda93ae81b2fa86f352c97922b8f54
Peter Szabo <pts@fazekas.hu>
2017-02-14 17:35:08 +0100

    fixed getenv (previous implementation was buggy)

--- src/misc/getenv.c
+++ src/misc/getenv.c
@@ -3,11 +3,13 @@
 char *getenv(const char *name) {
   const char *p;
   for (p = name; *p != '\0' && *p != '='; ++p) {}
-  if (environ && *name != '\0' && *p == '=') {
+  if (environ && *name != '\0' && *p != '=') {
     char **e;
-    const size_t len = p + 1 - name;
-    for (e = environ; *e && 0 != memcmp(*e, name, len); ++e) {}
-    if (*e) return *e + len;
+    const size_t len = p - name;
+    for (e = environ;
+         *e && (0 != memcmp(*e, name, len) || e[0][len] != '=');
+         ++e) {}
+    if (*e) return *e + len + 1;
   }
   return NULL;
 }
