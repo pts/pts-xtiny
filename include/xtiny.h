13886acf1e8735cfcf5a79c5e91bfc4808caf2ef
Peter Szabo <pts@fazekas.hu>
2017-12-22 09:50:53 +0100

    added more stat macros; added fstat

--- include/xtiny.h
+++ include/xtiny.h
@@ -713,14 +713,12 @@ __asm__ __volatile__ ("push %%ebp ; movl %%eax,%%ebp ; movl %1,%%eax ; int $0x80
 __syscall_return(type,__res); \
 }
 
-/* gcc-4.4 needs these forward-declarations */
+/* gcc-4.4 and sometimes gcc-4.8.4 needs these forward-declarations. */
 struct stat;
 struct stat64;
 struct timeval;
 struct timezone;
 struct utsname;
-
-/* --- System calls. */
 
 _syscall1_nomemory(int,close,int,fd)
 /* libc also allows a 2-argument open, but we don't (for tiny output).
@@ -755,6 +753,7 @@ _syscall3_nomemory(off_t,lseek,int,fd,off_t,offset,int,whence)
 _syscall5(int,_llseek,int,fd,int,offset_high,int,offset_low,loff_t*,result,int,whence);
 _syscall2(int,lstat,const char*,path,struct stat*,buf)
 _syscall2(int,stat,const char*,path,struct stat*,buf)
+_syscall2(int,fstat,int,fd,struct stat*,buf)
 _syscall2(int,mkdir,const char*,pathname,mode_t,mode)
 _syscall2(int,symlink,const char*,oldpath,const char*,newpath)
 _syscall1(int,unlink,const char*,pathname)
@@ -977,7 +976,7 @@ static __inline__ int puts(const char *__s) {
 #define SIGRTMIN	32
 #define SIGRTMAX	_NSIG
 
-/* --- */
+/* --- <sys/stat.h> macros */
 
 #define S_IFMT  0170000
 #define S_IFDIR 0040000
@@ -995,6 +994,25 @@ static __inline__ int puts(const char *__s) {
 #define S_ISFIFO(mode) (((mode) & S_IFMT) == S_IFIFO)
 #define S_ISLNK(mode)  (((mode) & S_IFMT) == S_IFLNK)
 #define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
+
+#define S_ISUID 04000
+#define S_ISGID 02000
+#define S_ISVTX 01000
+
+#define S_IRWXU 00700
+#define S_IRUSR 00400
+#define S_IWUSR 00200
+#define S_IXUSR 00100
+
+#define S_IRWXG 00070
+#define S_IRGRP 00040
+#define S_IWGRP 00020
+#define S_IXGRP 00010
+
+#define S_IRWXO 00007
+#define S_IROTH 00004
+#define S_IWOTH 00002
+#define S_IXOTH 00001
 
 /* --- */
 
@@ -1024,7 +1042,6 @@ static __inline__ int puts(const char *__s) {
 #define SEEK_SET 0
 #define SEEK_CUR 1
 #define SEEK_END 2
-
 /* --- structs
  *
  * Please note that struct stat etc. defined in
